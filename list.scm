61d3529b0024eb925d74dfe9ed0a4c5bcc7d5d2a
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2011-06-12 00:40:51 -0700

    Improve list.scm
    Don't crash on errors.

--- list.scm
+++ list.scm
@@ -1,15 +1,20 @@
 (use-modules (ice-9 format))
 
-(define filename "pokegra-hg.narc")
+(define filename "pokegra-w.narc")
+
+(define (try-load-file narc i)
+  (catch 'narc-error
+         (lambda () (narc-load-file narc i))
+         (lambda (key . args) #f)))
 
 (let* ((narc (load-narc filename))
        (count (narc-file-count narc)))
   (do ((i 0 (1+ i)))
       ((>= i count))
     (format #t "~3d ~a~%" i
-      (if (zero? (narc-get-file-size narc i))
-        "(empty)"
-        (let ((chunk (narc-load-file narc i)))
-          (if chunk
-              (get-magic chunk)
+      (let ((chunk (try-load-file narc i)))
+        (if chunk
+            (get-magic chunk)
+            (if (zero? (narc-get-file-size narc i))
+              "(empty)"
               "(error)"))))))
